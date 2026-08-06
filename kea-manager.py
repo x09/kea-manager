8f5acdda02f2500e1de57ebaf3a017c22c075790
Anton Shevtsov <shevtsov.anton@gmail.com>
2026-08-06 11:23:22 +0500

    fix module path

--- kea-manager.py
+++ kea-manager.py
@@ -15,7 +15,23 @@ import os
 import sys
 
 # гарантируем, что пакет kea_manager найдётся независимо от cwd
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+#sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
+_CANDIDATE_DIRS = [
+    os.path.dirname(os.path.abspath(__file__)),
+    os.path.dirname(os.path.realpath(__file__)),
+    "/usr/share/kea-manager",
+    "/usr/local/share/kea-manager",
+    os.path.expanduser("~/.local/share/kea-manager"),
+]
+
+for _d in _CANDIDATE_DIRS:
+    if os.path.isfile(os.path.join(_d, "kea_manager", "__init__.py")):
+        if _d not in sys.path:
+            sys.path.insert(0, _d)
+        break
+
+
 
 from kea_manager.__main__ import main
 
